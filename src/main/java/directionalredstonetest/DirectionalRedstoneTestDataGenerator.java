095da8f721303ba2132ff0731046eeea115a9d05
cputnam-a11y <chrlsvryptnm@gmail.com>
2024-11-05 14:25:14 -0600

    Initial commit

---
+++ src/main/java/directionalredstonetest/DirectionalRedstoneTestDataGenerator.java
@@ -0,0 +1,11 @@
+package directionalredstonetest;
+
+import net.fabricmc.fabric.api.datagen.v1.DataGeneratorEntrypoint;
+import net.fabricmc.fabric.api.datagen.v1.FabricDataGenerator;
+
+public class DirectionalRedstoneTestDataGenerator implements DataGeneratorEntrypoint {
+	@Override
+	public void onInitializeDataGenerator(FabricDataGenerator fabricDataGenerator) {
+
+	}
+}
